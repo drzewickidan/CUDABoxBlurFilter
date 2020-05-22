f75afaa3a35dcb2575a975667728aa7b2d8f7c7a
djd378 <djd378@xunil-05.coe.drexel.edu>
2020-05-22 17:27:39 -0400

    Initial commit.

---
+++ blur_filter_kernel.cu
@@ -0,0 +1,44 @@
+/* Blur filter. Device code. */
+
+#ifndef _BLUR_FILTER_KERNEL_H_
+#define _BLUR_FILTER_KERNEL_H_
+
+#include "blur_filter.h"
+
+__global__ void 
+blur_filter_kernel (const float *in, float *out, int size)
+{
+    int pix, i, j;
+    int row, col;
+    int curr_row, curr_col;
+    float blur_value;
+    int num_neighbors;
+
+	row = blockIdx.x * blockDim.x + threadIdx.x;             /* Obtain row number of pixel */
+	col = blockIdx.y * blockDim.y + threadIdx.y;           /* Obtain column number of pixel */
+	pix = row * size + col;
+
+	/* Apply blur filter to current pixel */
+	blur_value = 0.0;
+	num_neighbors = 0;
+	for (i = -BLUR_SIZE; i < (BLUR_SIZE + 1); i++) {
+		for (j = -BLUR_SIZE; j < (BLUR_SIZE + 1); j++) {
+			/* Accumulate values of neighbors while checking for 
+			 * boundary conditions */
+			curr_row = row + i;
+			curr_col = col + j;
+			if ((curr_row > -1) && (curr_row < size) &&\
+					(curr_col > -1) && (curr_col < size)) {
+				blur_value += in[curr_row * size + curr_col];
+				num_neighbors += 1;
+			}
+		}
+	}
+
+	/* Write averaged blurred value out */
+	out[pix] = blur_value/num_neighbors;
+    
+    return;
+}
+
+#endif /* _BLUR_FILTER_KERNEL_H_ */
